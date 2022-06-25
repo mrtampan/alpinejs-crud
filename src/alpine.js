738bf88ec764d809cfd06915390565c44d3c0e9a
achmadrivaldi <mrtampan54@gmail.com>
2022-06-26 02:50:36 +0700

    update lebih baik dari sebelumnya

--- src/alpine.js
+++ src/alpine.js
@@ -2,16 +2,29 @@ import Alpine from 'alpinejs'
  
 window.Alpine = Alpine
 
-Alpine.data('dropdown', () => ({
+Alpine.data('myData', () => ({
           open: false,
           toggle() {
             this.open = ! this.open
-          }
-}),
-'myData', () => ({
-          firstname: 'heheh',
-          lastname: 'huhuh'
-}),
+          },
+          firstname: 'Chou',
+          lastname: 'Tzuyu',
+          gender: 'perempuan',
+          hbdOpen: false,
+          hbdTrigger(){
+            this.hbdOpen = ! this.hbdOpen
+          },
+          memberGroup:[
+            { id: 1, nama: 'Jihyo' },
+            { id: 2, nama: 'Sana' },
+            { id: 3, nama: 'Mina' },
+            { id: 4, nama: 'Momo' },
+            { id: 5, nama: 'Chaeyoung' },
+            { id: 6, nama: 'Nayeon' },
+            { id: 7, nama: 'Dahyun' },
+            { id: 8, nama: 'Jeongyeon' },
+        ]
+})
 )
 
 Alpine.start()
